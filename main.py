ad9ed9dc90262b0121cf9c52c2ebcb7f6360d532
Monali Banerjee <70104314+monalibanerjee@users.noreply.github.com>
2022-09-01 00:20:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ from scoreboard import Scoreboard
 import time
 
 screen = Screen()
-screen.setup(width=1000, height=600)
+screen.setup(width=600, height=600)
 screen.bgcolor("black")
-screen.title("Snake")
+screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
@@ -33,18 +33,18 @@ while game_is_on:
         scoreboard.increase_score()
 
     #Detect collision with wall.
-    if snake.head.xcor() > 480 or snake.head.xcor() < -480 or snake.head.ycor() > 295 or snake.head.ycor() < -295:
-        game_is_on = False
-        scoreboard.game_over()
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        scoreboard.reset()
+        snake.reset()
+
 
     #Detect collision with tail.
     for segment in snake.segments:
         if segment == snake.head:
             pass
         elif snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
-
+            scoreboard.reset()
+            snake.reset()
 
 
 
